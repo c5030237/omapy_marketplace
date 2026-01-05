5dd02821d0422c77a55f88a3e95817856f87b1a1
c5030237 <c5030237@hallam.shu.ac.uk>
2026-01-05 22:42:38 +0000

    created database, db.py file,Init_db.py, schema and test.py to help connect the website with a database

---
+++ db/test_data.py
@@ -0,0 +1,42 @@
+# Example film data for testing
+products_data = [
+    {
+        'id': 1,         
+        'user': 1,
+        'product_name': 'Foam',
+        'product_price': 5000,
+        'product_category': 'Accessories',
+        'product_brand': 'Mouka',
+        'product_description': 'Enjoy rest and sleep with this ',
+        'product_image': 'https://www.bedstar.co.uk/media/catalog/product/cache/28d91bd31eb519c851f706c4c242d7a4/m/a/mayfair.jpg',
+        'product_gallery1': 'https://dreamersbedcentre.co.uk/wp-content/uploads/2024/09/TRILOGY-LIFESTYLE-B.jpg.webp',
+        'product_gallery2': 'https://www.bestbedsdirect.co.uk/user/products/large/adjust-a-bed-marino-1000-pocket-memory-adjustable-bed.jpg',
+        'product_gallery3': 'https://www.bestbedsdirect.co.uk/user/products/large/adjust-a-bed-marino-1000-pocket-memory-adjustable-bed.jpg'
+    },
+    {
+        'id': 2,         
+        'user': 1,
+        'product_name': 'Laptop',
+        'product_price': 50000,
+        'product_category': 'Gadgets',
+        'product_brand': 'Dell',
+        'product_description': 'For School Projects, Fast and reliable',
+        'product_image': 'https://cdn.arstechnica.net/wp-content/uploads/2019/02/xps132019_15.jpg',
+        'product_gallery1': 'https://media.wired.com/photos/59dfbc3d6d52e55cdead6b1a/3:2/w_2560%2Cc_limit/laptop-FA.jpg',
+        'product_gallery2': 'https://gadgettgang.com/wp-content/uploads/2024/08/main-2-scaled.jpg',
+        'product_gallery3': 'https://cdn.images.express.co.uk/img/dynamic/59/1200x630/5625585.jpg'
+    },
+    {
+        'id': 3,         
+        'user': 1,
+        'product_name': 'Television',
+        'product_price': 40000,
+        'product_category': 'Electronics',
+        'product_brand': 'Samsung',
+        'product_description': 'Portable Television for student use',
+        'product_image': 'https://cdn.mos.cms.futurecdn.net/YaWSd7qbkHCBq7LSdrheM4-1200-80.jpg',
+        'product_gallery1': 'https://cdn.mos.cms.futurecdn.net/dm9qefzptw9gep3e33pFuK-1200-80.jpg',
+        'product_gallery2': 'https://www.reliant.co.uk/blog/wp-content/uploads/2024/02/smalltv.jpg',
+        'product_gallery3': 'https://img.global.news.samsung.com/global/wp-content/uploads/2015/07/Main_Thumb_0708.jpg'
+    }
+]
